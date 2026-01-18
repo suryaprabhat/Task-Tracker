773a0a090539b4a7351a848ba57923fc8a9a5940
suryaprabhat <maddalasuryaprabhat@gmail.com>
2026-01-18 20:27:11 +0530

    first

--- server/index.js
+++ server/index.js
@@ -11,6 +11,7 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:5173",
   "https://task-tracker-ec3o.vercel.app",
+  "http://task-tracker-five-zeta.vercel.app"
 ];
 
 app.use(
